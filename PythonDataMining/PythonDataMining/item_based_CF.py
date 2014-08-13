708a9feefc3f1ddcdbcd3f4ef8bd69d0ab93e58e
Shuo Lv <claus.lv@hotmail.com>
2014-08-13 21:04:57 +0800

    Adjust Cosine Similiarty Algrithm
    to be continue

--- PythonDataMining/PythonDataMining/item_based_CF.py
+++ PythonDataMining/PythonDataMining/item_based_CF.py
@@ -20,3 +20,30 @@ users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones":
 }
 
 ## To Do
+## Test Dataset
+users = {"David":{"Imagine Dragons": 3, "Daft Punk": 5, "Lorde": 4, "Fall Out Boy": 1},
+         "Matt":{"Imagine Dragons": 3, "Daft Punk": 4, "Lorde": 4, "Fall Out Boy": 1},
+         "Ben":{"Kacey Musgraves": 4, "Imagine Dragons": 3, "Lorde": 3, "Fall Out Boy": 1},
+         "Chris":{"Kacey Musgraves": 4, "Imagine Dragons": 4, "Daft Punk": 4,
+                  "Lorde": 3, "Fall Out Boy": 1},
+         "Torri":{"Kacey Musgraves": 5, "Imagine Dragons": 4, "Daft Punk": 5, "Fall Out Boy": 3}
+}
+
+## Cosine Similiarty
+def computeSimiliarty(band1, band2, data):
+    averages = {}
+    for (key, ratings) in data.items():
+        averages[key] = float(sum(ratings.values())) / len(ratings.values())
+
+    num = 0
+    dem1 = 0
+    dem2 = 0
+    for (key, ratings) in data.items():
+        if band1 in ratings and band2 in ratings:
+            avg = averages[key]
+            num += (ratings[band1] - avg) * (ratings[band2] - avg)
+            dem1 += (ratings[band1] - avg) ** 2
+            dem2 += (ratings[band2] - avg) ** 2
+    return num / (sqrt(dem1) * sqrt(dem2))
+
+print computeSimiliarty("Kacey Musgraves","Fall Out Boy", users)

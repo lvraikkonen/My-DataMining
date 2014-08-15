40dbe6b0394b61c26cef5bfa24c0a84b10e8f085
Shuo Lv <claus.lv@hotmail.com>
2014-08-15 18:35:04 +0800

    Weighted Slope One

--- PythonDataMining/PythonDataMining/item_based_CF.py
+++ PythonDataMining/PythonDataMining/item_based_CF.py
@@ -19,7 +19,6 @@ users = {"Angelica": {"Blues Traveler": 3.5, "Broken Bells": 2.0, "Norah Jones":
                       "Slightly Stoopid": 2.5, "The Strokes": 3.0} 
 }
 
-## To Do
 ## Test Dataset
 users = {"David":{"Imagine Dragons": 3, "Daft Punk": 5, "Lorde": 4, "Fall Out Boy": 1},
          "Matt":{"Imagine Dragons": 3, "Daft Punk": 4, "Lorde": 4, "Fall Out Boy": 1},
@@ -29,21 +28,86 @@ users = {"David":{"Imagine Dragons": 3, "Daft Punk": 5, "Lorde": 4, "Fall Out Bo
          "Torri":{"Kacey Musgraves": 5, "Imagine Dragons": 4, "Daft Punk": 5, "Fall Out Boy": 3}
 }
 
-## Cosine Similiarty
-def computeSimiliarty(band1, band2, data):
-    averages = {}
-    for (key, ratings) in data.items():
-        averages[key] = float(sum(ratings.values())) / len(ratings.values())
+users2 = {"Amy": {"Taylor Swift": 4, "PSY": 3, "Whitney Houston": 4},
+          "Ben": {"Taylor Swift": 5, "PSY": 2},
+          "Clara": {"PSY": 3.5, "Whitney Houston": 4},
+          "Daisy": {"Taylor Swift": 5, "Whitney Houston": 3}}
 
-    num = 0
-    dem1 = 0
-    dem2 = 0
-    for (key, ratings) in data.items():
-        if band1 in ratings and band2 in ratings:
-            avg = averages[key]
-            num += (ratings[band1] - avg) * (ratings[band2] - avg)
-            dem1 += (ratings[band1] - avg) ** 2
-            dem2 += (ratings[band2] - avg) ** 2
-    return num / (sqrt(dem1) * sqrt(dem2))
 
-print computeSimiliarty("Kacey Musgraves","Fall Out Boy", users)
+### Cosine Similiarty
+#def computeSimiliarty(band1, band2, data):
+#    averages = {}
+#    for (key, ratings) in data.items():
+#        averages[key] = float(sum(ratings.values())) / len(ratings.values())
+#    #print "averages is: ", averages
+
+#    num = 0
+#    dem1 = 0
+#    dem2 = 0
+#    for (key, ratings) in data.items():
+#        if band1 in ratings and band2 in ratings:
+#            avg = averages[key]
+#            num += (ratings[band1] - avg) * (ratings[band2] - avg)
+#            dem1 += (ratings[band1] - avg) ** 2
+#            dem2 += (ratings[band2] - avg) ** 2
+#    return num / (sqrt(dem1) * sqrt(dem2))
+
+#print computeSimiliarty("Kacey Musgraves","Lorde", users)
+#print computeSimiliarty("Imagine Dragons","Lorde", users)
+#print computeSimiliarty("Daft Punk","Lorde", users)
+
+
+## Weighted Slope One Algroithm
+## 1. Compute deviations
+def computeDevation(data):
+    count = {}
+    dev = {}
+
+    ## each user ratings
+    for ratings in data.values(): # each user ratings
+        for (item, rating) in ratings.items(): # baseline rating (to be compared)
+            #count = 0
+            #dev = 0
+            count.setdefault(item, {})
+            dev.setdefault(item, {})
+            for (item2, rating2) in ratings.items(): # compare
+
+                if item2 != item: ## different band
+                    count[item].setdefault(item2,0)
+                    dev[item].setdefault(item2, 0.0)
+                    count[item][item2] += 1
+                    dev[item][item2] += rating - rating2
+                else: #same band
+                    count[item][item2] = 0
+                    dev[item][item2] = 0.0
+
+    for (item, ratings) in dev.items():
+        for item2 in ratings:
+            if count[item][item2] != 0:
+                ratings[item2] /= count[item][item2]
+            else:
+                ratings[item2] = 0
+    #print "count: ",count
+    #print "dev is: ",dev
+    return dev,count
+    
+## print computeDevation(users2)
+
+## 2. Predict
+def slopeOneRecommendations(data, deviations, count, user, band):
+    if data[user].get(band) <> None:
+        return data[user][band]
+    itemPrediction = 0
+    totalFrequency = 0
+    for key in data[user]:
+        if key != band:
+            itemPrediction += (data[user][key] + deviations[band][key]) * count[band][key]
+            totalFrequency += count[band][key]
+        itemPrediction /= totalFrequency
+    return itemPrediction  
+
+d,c = computeDevation(users2)
+print "Devation: ",d
+print "Count: ",c
+predict_HOU_Ben = slopeOneRecommendations(users2, d,c , "Ben", "Whitney Houston")
+print predict_HOU_Ben 
--- README.md
+++ README.md
@@ -3,7 +3,31 @@ My-DataMining
 
 Some DataMining Case Study
 
-##Classification with R
+## Python 实现
+###协同过滤推荐
+协同过滤推荐（Collaborative Filtering recommendation）是在信息过滤和信息系统中正迅速成为一项很受欢迎的技术。与传统的基于内容过滤直接分析内容进行推荐不同，协同过滤分析用户兴趣，在用户群中找到指定用户的相似（兴趣）用户，综合这些相似用户对某一信息的评价，形成系统对该指定用户对此信息的喜好程度预测。
+ 与传统文本过滤相比，协同过滤有下列优点:
+1 能够过滤难以进行机器自动基于内容分析的信息。如艺术品、音乐；
+2 能够基于一些复杂的，难以表达的概念（信息质量、品位)进行过滤；
+3 推荐的新颖性。
+ 尽管协同过滤技术在个性化推荐系统中获得了极大的成功，但随着站点结构、内容的复杂度和用户人数的不断增加，协同过滤技术的一些缺点逐渐暴露出来。
+ 主要有以下三点:
+1 稀疏性(sparsity)：在许多推荐系统中，每个用户涉及的信息量相当有限，在一些大的系统如亚马逊网站中，用户最多不过就评估了上百万本书的1%~2%。造成评估矩阵数据相当稀疏，难以找到相似用户集，导致推荐效果大大降低。
+2 扩展性(scalability)：“最近邻居”算法的计算量随着用户和项的增加而大大增加，对于上百万之巨的数目，通常的算法将遭遇到严重的扩展性问题。
+3 精确性(accuracy)：通过寻找相近用户来产生推荐集，在数量较大的情况下，推荐的可信度随之降低。
+
+* User-Based
+相似性判别方法：
+- 距离方法(`Minkowski Metrics`)
+- 皮尔逊相关性
+- 余弦相似性 
+
+* Item-Based
+- 调整余弦相似性(`Adjust Cosine Similiarty`)
+- `Slope One`算法
+
+## R
+### Classification with R
 
 * Decision trees: rpart, party
 * Random forest: randomForest, party
@@ -11,20 +35,19 @@ Some DataMining Case Study
 * Neural networks: nnet, neuralnet, RSNNS
 * Performance evaluation: ROCR
 
-
-##Clustering with R
+### Clustering with R
 * k-means: kmeans(), kmeansruns()9
 * k-medoids: pam(), pamk()
 * Hierarchical clustering: hclust(), agnes(), diana()
 * DBSCAN: fpc
 * BIRCH: birch
 
-##Association Rule Mining with R
+### Association Rule Mining with R
 * Association rules: apriori(), eclat() in package arules
 * Sequential patterns: arulesSequence
 * Visualisation of associations: arulesViz
 
-##Text Mining with R
+### Text Mining with R
 * Text mining: tm
 * Topic modelling: topicmodels, lda
 * Word cloud: wordcloud
